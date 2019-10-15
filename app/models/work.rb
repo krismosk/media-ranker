e7808c43621d4d236e1639c67af19cacd5fd546e
Kristina <kristina.tanya@gmail.com>
2019-10-15 10:35:47 -0700

    media spotlight with random selection created

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,12 @@ class Work < ApplicationRecord
   has_many :votes
   validates :category, :creator, :publication_year, :description, presence: true
   validates :title, presence: true, uniqueness: true
+
+  # pre-votes implementation
+  def self.select_spotlight
+    spotlight_work = Work.all.sample
+    return spotlight_work
+  end
+
+
 end
