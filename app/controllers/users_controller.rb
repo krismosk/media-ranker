d6dbabd846d667c73c6ab94d7e86dc4e230e1577
Kristina <kristina.tanya@gmail.com>
2019-10-15 13:53:21 -0700

    created a new user log in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
-end
+  
+  def login_form
+    @user = User.new
+  end
+  
+  def login
+  end 
+
+  def logout
+  end
+
+  def current
+  end
+
+
+
+end 
