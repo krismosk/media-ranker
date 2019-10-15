55d746ce73fdf7b22a7ee0b19c558da89c3c6fd3
Kristina <kristina.tanya@gmail.com>
2019-10-15 15:21:14 -0700

    outlined describe and it blocks for work model test

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "can be instantiated" do
+  end
+
+  it "will have the required fields" do
+  end
+
+  describe "relationships" do
+    it "can have many votes" do
+    end 
+  end 
+
+  describe "validations" do
+    it "must have a category" do
+    end 
+    
+    it "must have a title" do
+    end 
+
+    it "must have a unique title" do
+    end
+
+    it "must have a creator" do
+    end
+
+    it "must have a publication year" do
+    end 
+
+    it "must have a description" do
+    end
+  end
+
+  describe "custom methods" do
+    describe "#select_spotlight" do
+      it "return a randomly selected media" do
+      end
+    end
+
+    describe "#select_top_ten" do
+      # add it blocks for nominal and edge cases
+    end
+  end
+
+
+
 end
