18b015579e9556f40ed4750860da375c4f36a062
Kristina <kristina.tanya@gmail.com>
2019-10-15 13:46:55 -0700

    Created user table, added username column, created controller and cutom routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'works#home'
   resources :works
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
---
+++ db/migrate/20191015204103_add_user_table.rb
@@ -0,0 +1,8 @@
+class AddUserTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.timestamps
+    end
+  end
+end
