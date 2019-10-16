89333b1fc6c9c58ab216412ea79c0e93fafda618
Kristina <kristina.tanya@gmail.com>
2019-10-16 14:41:01 -0700

    add votes history to a individual works show page

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  has_many :votes, :through => :users
+  has_many :votes
   validates :category, :creator, :publication_year, :description, presence: true
   validates :title, presence: true, uniqueness: true
 
