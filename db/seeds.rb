a898bfdbb36b06427780abd4f56a8e1d5b137cb1
Kristina <kristina.tanya@gmail.com>
2019-10-15 21:26:54 -0700

    added seed code for users and works and new csv files for both

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,5 +20,28 @@ CSV.foreach(WORK_FILE, :headers => true) do |row|
   end
 end
 
-puts "Added #{Work.count} workrecords"
+puts "Added #{Work.count} work records"
 puts "#{work_failures.length} works failed to save"
+
+# seed users
+USER_FILE = Rails.root.join('db', 'seed_data', 'users.csv')
+puts "Loading raw driver data from #{USER_FILE}"
+
+user_failures = []
+CSV.foreach(USER_FILE, :headers => true) do |row|
+  user = User.new
+  user.username = row['username']
+  user.joined_date = row['joined_date']
+  successful = user.save
+  if !successful
+    user_failures << user
+    puts "Failed to save user: #{user.inspect}"
+  else
+    puts "Created user: #{user.inspect}"
+  end
+end
+
+puts "Added #{User.count} user records"
+puts "#{user_failures.length} users failed to save"
+
+# seed votes
