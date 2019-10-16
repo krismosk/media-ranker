adcf455c4e637ed297ccbba3b1e4e1f546ec5dff
Kristina <kristina.tanya@gmail.com>
2019-10-16 09:13:12 -0700

    Create a new relationship: Work has many votes through users

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  has_many :votes
+  has_many :votes, :through => :users
   validates :category, :creator, :publication_year, :description, presence: true
   validates :title, presence: true, uniqueness: true
 
