63d4ba4a66fec52cb20e5ef041e64d070f78b349
Kristina <kristina.tanya@gmail.com>
2019-10-16 22:33:53 -0700

    vote instantiation and field presence tests passing

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -2,9 +2,48 @@ require "test_helper"
 
 describe Vote do
   it "can be instantiated" do 
+    user = User.create(
+      username: "kmosk",
+      joined_date: Time.now,
+    )
+    work = Work.create(
+      category: "movie",
+      title: "Little Ladies",
+      creator: "Benedict Sadat",
+      publication_year: 1994,
+      description: "Persistent even-keeled toolset",
+    )
+    vote = Vote.create(
+      date: Time.now,
+      user_id: user.id,
+      work_id: work.id, 
+    )
+
+    expect(vote.valid?).must_equal true
   end 
 
   it "will have the required fields" do
+    user = User.create(
+      username: "kmosk",
+      joined_date: Time.now,
+    )
+    work = Work.create(
+      category: "movie",
+      title: "Little Ladies",
+      creator: "Benedict Sadat",
+      publication_year: 1994,
+      description: "Persistent even-keeled toolset",
+    )
+    vote = Vote.create(
+      date: Time.now,
+      user_id: user.id,
+      work_id: work.id, 
+    )
+
+    vote = Vote.first
+    [:date, :user_id, :work_id].each do |field|
+      expect(vote).must_respond_to field
+    end
   end 
 
   describe "validations" do
@@ -25,8 +64,5 @@ describe Vote do
     it "belongs to a work" do
     end
   end
-
-
-
 
 end
