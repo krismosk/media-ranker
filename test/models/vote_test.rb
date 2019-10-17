d04e1e0694bce599b6cbd852e42263a403ca15e2
Kristina <kristina.tanya@gmail.com>
2019-10-16 22:42:06 -0700

    vote validations tests passing

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -47,13 +47,43 @@ describe Vote do
   end 
 
   describe "validations" do
+    before do
+      @user = User.create(
+        username: "kmosk",
+        joined_date: Time.now,
+      )
+      @work = Work.create(
+        category: "movie",
+        title: "Little Ladies",
+        creator: "Benedict Sadat",
+        publication_year: 1994,
+        description: "Persistent even-keeled toolset",
+        )
+      @vote = Vote.create(
+        date: Time.now,
+        user_id: @user.id,
+        work_id: @work.id, 
+      )
+    end
     it "must have a date" do
+      @vote.date = nil
+      expect(@vote.valid?).must_equal false
+      expect(@vote.errors.messages).must_include :date
+      expect(@vote.errors.messages[:date]).must_equal ["can't be blank"]
     end
 
     it "must have a user" do
+      @vote.user_id = nil
+      expect(@vote.valid?).must_equal false
+      expect(@vote.errors.messages).must_include :user
+      expect(@vote.errors.messages[:user]).must_equal ["must exist", "can't be blank"]
     end
 
     it "must have a work" do
+      @vote.work_id = nil
+      expect(@vote.valid?).must_equal false
+      expect(@vote.errors.messages).must_include :work
+      expect(@vote.errors.messages[:work]).must_equal ["must exist", "can't be blank"]
     end
   end
 
