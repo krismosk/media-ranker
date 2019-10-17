8312d280d5031927b86050336a72b01aab6df63c
Kristina <kristina.tanya@gmail.com>
2019-10-16 22:00:27 -0700

    modify work model method to chose the top ten works of each category with the highest vote count (sort in descending order)

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,45 +3,26 @@ class Work < ApplicationRecord
   validates :category, :creator, :publication_year, :description, presence: true
   validates :title, presence: true, uniqueness: true
 
-  # pre-votes implementation
   def self.select_spotlight
-    # works.max_by
-    # works.vount.count
     works = Work.all
     spotlight_work = works.max_by { |work| work.votes.length }
-
     return spotlight_work
   end
 
-  # pre-votes implementation
-  # TO-DO: add logic to deal with no records returned
   def self.select_top_ten(category)
     result = Work.where(category: category)
-    # if result = []
-    # return result
-    # else
-    # works.sort_by do |work|
-    #  -work.votes.count
-    # end
-
-    # use .take(10) to pick the top 10
     if result.empty?
       return nil
-    end
-    
-    # if there is less than 10 records in result, display all
-    if result.length < 10
-      return result
+    else
+      sorted_works = result.sort_by do |work|
+        -work.votes.count
+      end
+      if sorted_works.length > 10
+        top_ten = sorted_works.take(10)
+        return top_ten
+      else
+        return sorted_works
+      end
     end 
-
-    # if there's more than 10 records in result
-    # find the excess and remove those records from result
-    if result.length > 10
-      excess_works = result.length - 10
-      new_result = result.drop(excess_works)
-      return new_result
-    end
   end
-
-
 end
