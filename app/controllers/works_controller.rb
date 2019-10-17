d555560dfead38c1215337e102fafcb56e585f41
Kristina <kristina.tanya@gmail.com>
2019-10-16 21:37:27 -0700

    add logic to prevent user from voting for the same work more than once

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -68,19 +68,33 @@ class WorksController < ApplicationController
   end
 
   def upvote
-    work = Work.find_by(id: params[:id])
-    current_user = User.find_by(id: session[:user_id])
-    if current_user.nil?
+    @work = Work.find_by(id: params[:id])
+    @current_user = User.find_by(id: session[:user_id])
+
+    if @current_user.nil?
       flash[:error] = 'A problem occurred: You must be logged in to vote.'
-      redirect_to works_path
+      redirect_to work_path
       return
     end 
-    @vote = Vote.create(date: Time.now, user_id: current_user.id, work_id: work.id)
-    if @vote.save
-      flash[:success] = "Successfully upvoted."
+
+    if validate_vote == false
+      flash[:error] = 'A problem occurred: This work has already been upvoted.'
+      redirect_to work_path
+      return
+    else
+      @vote = Vote.create(date: Time.now, user_id: @current_user.id, work_id: @work.id)
+      flash[:success] = 'Successfully upvoted.'
       redirect_to work_path
     end
-  end 
+  end
+
+  def validate_vote 
+    @current_user.votes.each do |vote|
+      if vote.work_id == @work.id
+        return false
+      end
+    end
+  end
 
   private
 
